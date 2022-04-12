cf277e6cd037d109b7c9cd7fc5a480dc923307ec
regi <cabael.regine@gmail.com>
2022-04-12 14:57:27 +0200

    acqua

--- Csharp Shop 2/Acqua.cs
+++ Csharp Shop 2/Acqua.cs
@@ -8,5 +8,97 @@ namespace Csharp_Shop_2
 {
     internal class Acqua : Prodotto
     {
+        public double litri; //litri presenti nella bottiglia
+        private double ph; //livello di acidità dell'acqua
+        private string sorgente; //provenienza dell'acqua
+
+        //COSTRUTTORE senza iva e senza prezzo
+        public Acqua(string nome, string descrizione, double litri, double ph, string sorgente) : base(nome, descrizione)
+        {
+            this.litri = litri;
+            this.ph = ph;  
+            this.sorgente = sorgente;
+        }
+
+        //COSTRUTTORE con iva e prezzo
+        public Acqua(string nome, string descrizione, double prezzo, double iva, double litri, double ph, string sorgente) : base(nome, descrizione, prezzo, iva)
+        {
+            this.litri = litri;
+            this.ph=ph;
+            this.sorgente = sorgente;
+        }
+
+        //--------------GETTER E SETTER-------------------
+        public double GetPh()
+        {
+            return ph;
+        }
+
+        public string GetSorgente()
+        {
+            return sorgente;
+        }
+
+        //-------------METODI----------------
+        public void bevi(double litriDabere)
+        {
+            if(this.litri - litriDabere > 0)
+            {
+                this.litri -= litriDabere;
+                Console.WriteLine("Stai bevendo!");
+            }
+            else if(this.litri == 0)
+            {
+                Console.WriteLine("Non c'è abbastanza acqua!");
+            }
+            else
+            {
+                this.litri = 0;
+                Console.WriteLine("Hai finito tutta l'acqua!");
+            }
+        }
+
+        public void riempi(double litri)
+        {
+            if (litri == 0)
+            {
+                Console.WriteLine("Errore nel riempimento!");
+            } else if (this.litri + litri < 1.5)
+            {
+                this.litri += litri;
+                Console.WriteLine("Stai riempiendo la bottiglia!");
+            } else if (this.litri + litri > 1.5)
+            {
+                this.litri = 1.5;
+                Console.WriteLine("Hai riempito troppo la bottiglia!");
+            }
+        }
+
+        public void svuota()
+        {
+            if(this.litri == 0)
+            {
+                Console.WriteLine("La bottiglia è già vuota!");
+            }
+            else
+            {
+                this.litri = 0;
+                Console.WriteLine("Hai svuotato la bottiglia!");
+            }
+        }
+
+        public override void stampaProdotto()
+        {
+            base.stampaProdotto();
+            Console.WriteLine("Sorgente: " + this.sorgente);
+            Console.WriteLine("PH: " + this.ph);
+            Console.WriteLine("Litri: " + this.litri);
+        }
+
+        //METODO STATICO
+        public static double convertiInGalloni(double litri)
+        {
+            return litri * 3.785;
+        }
     }
 }
--- Csharp Shop 2/Prodotto.cs
+++ Csharp Shop 2/Prodotto.cs
@@ -8,71 +8,75 @@ namespace Csharp_Shop_2
 {
     internal class Prodotto
     {
-        internal class Prodotto
+        private int codice;
+        public string nome;
+        public string descrizione;
+        public double prezzo;
+        public double iva;
+
+
+        //COSTRUTTORE senza iva e senza prezzo
+
+        public Prodotto(string nome, string descrizione)
         {
-            private int codice;
-            public string nome;
-            public string descrizione;
-            public double prezzo;
-            public double iva;
+            this.nome = nome;
+            this.descrizione = descrizione;
+        }
+        //---COSTRUTTORE con iva e prezzo---
+        public Prodotto(string nome, string descrizione, double prezzo, double iva)
+        {
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            Random r = new Random(100000000);
+            codice = r.Next();
+            this.iva = iva;
+        }
 
-            //---COSTRUTTORE---
-            public Prodotto(string nome, string descrizione, double prezzo, double iva)
+
+        //---GETTER CODICE e BONUS
+        public string getCodice()
+        {
+            string codiceInStringa = codice.ToString();
+
+            if (codiceInStringa.Length != 8)
             {
-                this.nome = nome;
-                this.descrizione = descrizione;
-                this.prezzo = prezzo;
-                Random r = new Random(100000000);
-                codice = r.Next();
-                this.iva = iva;
-            }
-
-
-            //---GETTER CODICE e BONUS
-            public string getCodice()
-            {
-                string codiceInStringa = codice.ToString();
-
-                if (codiceInStringa.Length != 8)
+                while (codiceInStringa.Length < 8)
                 {
-                    while (codiceInStringa.Length < 8)
-                    {
-                        codiceInStringa = "0" + codiceInStringa;
-                    }
-                    return codiceInStringa;
+                    codiceInStringa = "0" + codiceInStringa;
                 }
-                else
-                {
-                    return codiceInStringa;
-                }
-
+                return codiceInStringa;
             }
-
-            public void stampaPrezzoBase()
+            else
             {
-                Console.WriteLine("Il prezzo di base è: " + prezzo + " euro");
+                return codiceInStringa;
             }
-
-            public void calcoloIva()
-            {
-                double risultato = Math.Round(prezzo * iva / 100 + prezzo, 2);
-                Console.WriteLine("Il prezzo con Iva è: " + risultato + " euro");
-            }
-
-            public void stampaNomeEsteso()
-            {
-                Console.WriteLine("Il nome esteso del prodotto è: " + getCodice() + " - " + nome);
-            }
-
-            public void stampaProdotto()
-            {
-                stampaNomeEsteso();
-                Console.WriteLine("Descrizione: " + descrizione);
-                stampaPrezzoBase();
-                calcoloIva();
-            }
-
 
         }
+
+        public void stampaPrezzoBase()
+        {
+            Console.WriteLine("Il prezzo di base è: " + prezzo + " euro");
+        }
+
+        public void calcoloIva()
+        {
+            double risultato = Math.Round(prezzo * iva / 100 + prezzo, 2);
+            Console.WriteLine("Il prezzo con Iva è: " + risultato + " euro");
+        }
+
+        public void stampaNomeEsteso()
+        {
+            Console.WriteLine("Il nome esteso del prodotto è: " + getCodice() + " - " + nome);
+        }
+
+        public virtual void stampaProdotto()
+        {
+            stampaNomeEsteso();
+            Console.WriteLine("Descrizione: " + descrizione);
+            stampaPrezzoBase();
+            calcoloIva();
+        }
+
     }
 }
